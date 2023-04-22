235c3e9cb872f1ac3d5caaa26adab391b8119369
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2023-04-22 18:45:23 -0400

    fix: type script name

--- build/listDevices.js
+++ build/listDevices.js
@@ -17,8 +17,8 @@ const cli_table3_1 = __importDefault(require("cli-table3"));
 const utils_1 = require("./utils");
 const showDevices = () => __awaiter(void 0, void 0, void 0, function* () {
     const tableDevices = new cli_table3_1.default({
-        head: ['UUID', 'Name', 'State'],
-        colWidths: [40, 50, 15],
+        head: ['UUID', 'Name', 'State', 'Version OS'],
+        colWidths: [40, 50, 15]
     });
     const command = 'xcrun simctl list devices --json';
     try {
@@ -29,12 +29,19 @@ const showDevices = () => __awaiter(void 0, void 0, void 0, function* () {
         }
         const data = JSON.parse(stdout);
         const devices = data['devices']['com.apple.CoreSimulator.SimRuntime.iOS-16-2'];
-        devices.forEach((device) => {
+        const items = Object.keys(data['devices'])
+            .map(runtime => {
+            const version = runtime.split('SimRuntime')[1].split('-');
+            return data['devices'][runtime].map((device) => (Object.assign(Object.assign({}, device), { versionOS: `${version[1]}.${version[2]}` })));
+        })
+            .flat()
+            .filter((item) => item.isAvailable);
+        items.forEach(device => {
             const name = `📱 ${device.name}`;
             const showState = device.state === 'Booted' ? '✅' : '❌';
-            tableDevices.push([device.udid, name, `${showState} ${device.state}`]);
+            tableDevices.push([device.udid, name, `${showState} ${device.state}`, device.versionOS]);
         });
-        console.log(tableDevices.slice(0, 5).toString());
+        console.log(tableDevices.toString());
     }
     catch (error) {
         if (error instanceof Error) {
--- src/listDevices.ts
+++ src/listDevices.ts
@@ -1,6 +1,6 @@
-import CliTable3 from "cli-table3";
+import CliTable3 from 'cli-table3';
 
-import { runCommand } from "./utils";
+import { runCommand } from './utils';
 
 interface SimulatorDevice {
   lastBootedAt: string;
@@ -17,8 +17,8 @@ interface SimulatorDevice {
 
 const showDevices = async () => {
   const tableDevices = new CliTable3({
-    head: ['UUID', 'Name', 'State'],
-    colWidths: [40, 50, 15],
+    head: ['UUID', 'Name', 'State', 'Version OS'],
+    colWidths: [40, 50, 15]
   });
 
   // list devices
@@ -34,22 +34,33 @@ const showDevices = async () => {
 
     const data = JSON.parse(stdout);
     const devices: SimulatorDevice[] = data['devices']['com.apple.CoreSimulator.SimRuntime.iOS-16-2'];
-    devices.forEach((device) => {
+
+    const items = Object.keys(data['devices'])
+      .map(runtime => {
+        const version = runtime.split('SimRuntime')[1].split('-');
+
+        return data['devices'][runtime].map((device: SimulatorDevice) => ({
+          ...device,
+          versionOS: `${version[1]}.${version[2]}`
+        }));
+      })
+      .flat()
+      .filter((item: SimulatorDevice) => item.isAvailable);
+
+    items.forEach(device => {
       const name = `📱 ${device.name}`;
       const showState = device.state === 'Booted' ? '✅' : '❌';
-      tableDevices.push([device.udid, name, `${showState} ${device.state}`]);
+      tableDevices.push([device.udid, name, `${showState} ${device.state}`, device.versionOS]);
     });
 
-    console.log(tableDevices.slice(0, 5).toString());
+    console.log(tableDevices.toString());
   } catch (error) {
     if (error instanceof Error) {
       console.error(`Error running command: ${error.message}`);
     } else {
       console.error(`Error running command: ${error}`);
     }
-  } 
-}
+  }
+};
 
-export {
-  showDevices
-}
+export { showDevices };
