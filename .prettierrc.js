ea2457a07f9d40e12965e626e38d56b4b4aa8f3a
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2023-04-22 16:37:24 -0400

    core: init project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+};
